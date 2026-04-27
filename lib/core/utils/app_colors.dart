8438d648d228c120be369f662ee51cbb9f61b485
Ahmed Gamal Shalaby <ahmedgamal@ gmail.com>
2026-04-28 02:14:45 +0300

    feat(ui): update primary app color

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF00335A);
+  static const Color primary = Color(0xFF0b2a4a);
   static const Color secondary = Color(0xFF25ADDE);
   static const Color background = Color(0xFFF5F5F5);
   static const Color surface = Colors.white;
