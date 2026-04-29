5442926dda7c98447c78860460b6c13b155a580c
Ahmed Gamal Shalaby <ahmedgamal@ gmail.com>
2026-04-30 02:12:19 +0300

    feat(home): convert news list to carousel with smooth page indicator
    Replace horizontal ListView with PageView.builder and
    SmoothPageIndicator for a swipeable news carousel.
    Also fix missing easy_localization import in home app bar.

--- lib/features/home/presentation/widgets/home_sliver_app_bar.dart
+++ lib/features/home/presentation/widgets/home_sliver_app_bar.dart
@@ -2,6 +2,7 @@ import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:easy_localization/easy_localization.dart';
 import '../../../../core/utils/app_colors.dart';
 import '../../../../core/utils/image_paths.dart';
 import '../../../../core/widgets/app_image.dart';
